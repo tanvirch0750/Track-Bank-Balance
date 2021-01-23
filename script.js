5b1ea582f813f4b5b495ba602dbadeb405aaae4a
tanvirch0750 <tanvir0750@yahoo.com>
2021-01-23 19:23:17 +0600

    create a DOM element. Displaying one user all movements using foreach method

--- script.js
+++ script.js
@@ -61,16 +61,22 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
+const displayMovements = movements => {
+  containerMovements.innerHTML = '';
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+  movements.forEach((mov, idx) => {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+    <div class="movements__row">
+      <div class="movements__type movements__type--${type} ${type}">${
+      idx + 1
+    } deposit</div>
+      <div class="movements__value">${mov}</div>
+    </div>
+    `;
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
 
-/////////////////////////////////////////////////
+displayMovements(account1.movements);
